f4fed688368f8873c664d850c85c96ff33319d0e
Matthieu Helbert <matthieuhelbert67@gmail.com>
2024-06-14 11:30:39 +0200

    method direction button N,S,E,W

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -11,20 +11,40 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/boat')]
 class BoatController extends AbstractController
 {
-    #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat')]
-    public function moveBoat(
-        int $x,
-        int $y,
-        BoatRepository $boatRepository,
-        EntityManagerInterface $entityManager
-    ): Response {
-        $boat = $boatRepository->findOneBy([]);
+    #[Route('/move/{x<\d+>}/{y<\d+>}', name: 'moveBoat', requirements: ['x'=>'\d+','y'=>'\d+'])]
+    public function moveBoat(int $x,int $y,BoatRepository $boatRepository,EntityManagerInterface $entityManager): Response
+    {
+        $boat = $boatRepository->findOneBy([$x,$y]);
         
         $boat->setCoordX($x);
         $boat->setCoordY($y);
 
         $entityManager->flush();
         
+        return $this->redirectToRoute('map');
+    }
+
+    #[Route('/direction/{direction}', name: 'moveDirection', requirements: ['direction'=>'[NSEW]'])]
+    public function moveDirection($direction, BoatRepository $boatRepository, EntityManagerInterface $entityManager, ): Response
+    {
+        $boat = $boatRepository->findOneBy([]);
+        $x = $boat->getCoordX();
+        $y = $boat->getCoordY();
+
+        switch ($direction) {
+            case 'N':
+                $boat->setCoordY($y - 1);
+                break;
+            case 'S':
+                $boat->setCoordY($y + 1);
+                break;
+            case 'E':
+                $boat->setCoordX($x - 1);
+                break;
+            case 'W':
+                $boat->setCoordX($x + 1);
+                break;
+        }
         return $this->redirectToRoute('map');
     }
 }
